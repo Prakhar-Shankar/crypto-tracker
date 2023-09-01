810de16f54e2b569c4860487456d3e1f39ba0d08
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-09-01 12:04:11 +0530

    signup thing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Featured from "./Components/Featured";
+import React from 'react'
+import Featured from './Components/Featured'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import Signup from './Components/Signup'
+
 
 function App() {
-    return(
-        <>
-        <Navbar/>
-        <Hero/>
-        <Featured/>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+      <Signup />
+      
+    </>
+  );
 }
 
 export default App;
